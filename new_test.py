9e0dc87dbe3231fb8495dab88f0c3ffd522d51f0
^_~Armatyra~_^ <galimovramil19@gmail.com>
2026-03-05 10:16:51 +0300

    update

--- new_test.py
+++ new_test.py
@@ -2,6 +2,7 @@ import pytest
 import torch
 from diffusers import StableDiffusionPipeline
 import numpy as np
+from PIL import Image
 
 MODEL_NAME = "Onodofthenorth/SD_PixelArt_SpriteSheet_Generator"
 
@@ -18,13 +19,24 @@ def test_model_and_processor_loading(model):
     assert model.unet is not None
     assert model.tokenizer is not None
 
-def test_inference_and_image_creation(model):
+def test_prompt_processing(model):
     prompt = "magical human, pixel art, white background"
-    output = model(
-        prompt
-    ).images[0]
+    inputs = model.tokenizer(
+        prompt,
+        return_tensors = "pt"
+    )
+    assert "input_ids" in inputs
+    assert inputs.input_ids.shape[1] > 0
 
-    assert output is not None
-    assert hasattr(output, "size")
+def test_image_generation_format(model):
+    prompt = "magical human, pixel art, white background"
+    output = model(prompt).images[0]
+    assert isinstance(output, Image.Image)
+    assert output.size == (512, 512)
+
+
+def test_image_is_not_empty(model):
+    prompt = "magical human, pixel art, white background"
+    output = model(prompt).images[0]
     img_array = np.array(output)
     assert img_array.std() > 0
